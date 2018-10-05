569eec539a4bdd0770ac375dce29a1ba39eacb2b
paulvvo <paulvo@live.ca>
2018-10-05 19:50:22 -0400

    finish modifying modals and added some animations to portfolio box and modals

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,10 @@ var modalContainer = document.getElementsByClassName("modalContainer")[0];
 var modalContent = document.getElementsByClassName("modalContent");
 var portfolioBox = document.getElementsByClassName("portfolio-box");
 
-// console.log(modalContent[0].textContent);
-// console.log(modalContent[1].textContent);
-
 for(let i=0; i<modalContent.length; i++){
 	portfolioBox[i].addEventListener("click", function(){
 		modalContainer.style.display="block";
-
 		modalContent[i].style.display="block";
-		console.log(modalContent[i].textContent);
-		console.log(i);
 	})
 }
 
@@ -21,7 +15,7 @@ window.addEventListener("click", function(e){
 		modalContainer.style.display="none";
 		for(var i=0; i<modalContent.length; i++){
 			if(modalContent[i].style.display ==="block"){
-				modalContent[i].style.display ==="none";
+				modalContent[i].style.display ="none";
 			}
 		}
 	}
