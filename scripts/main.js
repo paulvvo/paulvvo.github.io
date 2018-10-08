aea5b03791b9194c7e91d4b4f891b0debe721e22
paulvvo <paulvo@live.ca>
2018-10-07 21:45:46 -0400

    adjusted portfolio boxes and added all the new projects, adjusted all the links in the modals and adjusted the completed modal projects

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var modalContainer = document.getElementsByClassName("modalContainer")[0];
 var modalContent = document.getElementsByClassName("modalContent");
 var portfolioBox = document.getElementsByClassName("portfolio-box");
+var closeModalButton = document.querySelector("#closeModalButton");
+
 
 for(let i=0; i<modalContent.length; i++){
 	portfolioBox[i].addEventListener("click", function(){
@@ -9,7 +11,14 @@ for(let i=0; i<modalContent.length; i++){
 	})
 }
 
-
+closeModalButton.addEventListener("click", function(){
+	modalContainer.style.display="none";
+	for(var i=0; i<modalContent.length; i++){
+		if(modalContent[i].style.display ==="block"){
+			modalContent[i].style.display ="none";
+		}
+	}
+})
 window.addEventListener("click", function(e){
 	if(e.target === modalContainer){
 		modalContainer.style.display="none";
@@ -19,4 +28,4 @@ window.addEventListener("click", function(e){
 			}
 		}
 	}
-})
+});
