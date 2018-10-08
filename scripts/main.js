096228fa013c7ad233e2e00d728d67dcc89b4d27
paulvvo <paulvo@live.ca>
2018-10-07 23:21:05 -0400

    remove jquery smooth scrolling effect

--- scripts/main.js
+++ scripts/main.js
@@ -30,13 +30,13 @@ window.addEventListener("click", function(e){
 	}
 });
 
-$("document").ready(function(){
-	var scroll = $(".scroll");
-
-	scroll.click(function(event){
-		event.preventDefault();
-		$("body,html").animate({
-			scrollTop:$(this.hash).offset().top
-		}, 1000)
-	})
-})
+// $("document").ready(function(){
+// 	var scroll = $(".scroll");
+//
+// 	scroll.click(function(event){
+// 		event.preventDefault();
+// 		$("body,html").animate({
+// 			scrollTop:$(this.hash).offset().top
+// 		}, 1000)
+// 	})
+// })
